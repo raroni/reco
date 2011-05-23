f0ed86a8857691cd20af0e1010befcaf81b39078
rasmusrn <rasmusrnielsen@gmail.com>
2011-05-24 01:52:13 +0200

    Refactoring: DRYing up test_preprocessor.rb.

--- test/test_preprocessor.rb
+++ test/test_preprocessor.rb
@@ -2,11 +2,15 @@ require 'reco_test'
 
 class PreprocessorTest < Reco::TestCase
   def test_preprocessing_hello_eco_fixture
-    assert_equal fixture('hello.coffee'), Reco::Preprocessor.preprocess(fixture('hello.eco'))
+    assert_equal fixture('hello.coffee'), preprocess(fixture('hello.eco'))
   end
   
   def test_preprocessing_projects_eco_fixture
-    assert_equal fixture('projects.coffee'), Reco::Preprocessor.preprocess(fixture('projects.eco'))
+    assert_equal fixture('projects.coffee'), preprocess(fixture('projects.eco'))
   end
   
+  private
+  def preprocess(source)
+    Reco::Preprocessor.preprocess source
+  end
 end
