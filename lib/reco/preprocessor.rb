a0a1b754bd864c13cc0a822a9f1ca4d01dbd43f1
rasmusrn <rasmusrnielsen@gmail.com>
2011-05-24 02:53:49 +0200

    Better nesting so we don't have to write "Reco::".

--- lib/reco/preprocessor.rb
+++ lib/reco/preprocessor.rb
@@ -1,69 +1,71 @@
 require 'reco/scanner'
 require 'reco/util'
 
-class Reco::Preprocessor
-  def self.preprocess(source)
-    new(source).execute
-  end
+module Reco
+  class Preprocessor
+    def self.preprocess(source)
+      new(source).execute
+    end
   
-  def initialize(source)
-    @scanner = Reco::Scanner.new source
-    @output = ''
-    @level = 0
-    @options = {}
-    @captures = []
-  end
+    def initialize(source)
+      @scanner = Scanner.new source
+      @output = ''
+      @level = 0
+      @options = {}
+      @captures = []
+    end
   
-  def execute
-    @scanner.scan { |token| send token.shift, *token } until @scanner.done?
-    @output
-  end
+    def execute
+      @scanner.scan { |token| send token.shift, *token } until @scanner.done?
+      @output
+    end
   
-  def record(line)
-    @output += @level.times.collect { "  " }.join
-    @output += "#{line}\n"
-  end
+    def record(line)
+      @output += @level.times.collect { "  " }.join
+      @output += "#{line}\n"
+    end
   
-  def print_string(string)    
-    record "__out.push #{Reco::Util.inspect_string(string)}" unless string.empty?
-  end
+    def print_string(string)    
+      record "__out.push #{Reco::Util.inspect_string(string)}" unless string.empty?
+    end
   
-  def begin_code(options)
-    @options = options
-  end
+    def begin_code(options)
+      @options = options
+    end
   
-  def record_code(code)
-    if code != 'end'
-      if @options[:print]
-        record @options[:safe] ? "__out.push #{code}" : "__out.push __sanitize #{code}"
-      else
-        record code
+    def record_code(code)
+      if code != 'end'
+        if @options[:print]
+          record @options[:safe] ? "__out.push #{code}" : "__out.push __sanitize #{code}"
+        else
+          record code
+        end
       end
     end
-  end
   
-  def indent(capture = nil)
-    @level += 1
-    if capture
-      record "__capture #{capture}"
-      @captures.unshift @level
-      indent
+    def indent(capture = nil)
+      @level += 1
+      if capture
+        record "__capture #{capture}"
+        @captures.unshift @level
+        indent
+      end
     end
-  end
   
-  def dedent
-    @level -= 1
-    fail "unexpected dedent" if @level < 0
-    if @captures[0] == @level
-      @captures.shift
-      dedent
+    def dedent
+      @level -= 1
+      fail "unexpected dedent" if @level < 0
+      if @captures[0] == @level
+        @captures.shift
+        dedent
+      end
     end
+  
+    def fail(message)
+      raise Error, message
+    end
+  
+    class Error < Exception; end
+  
   end
-  
-  def fail(message)
-    raise Error, message
-  end
-  
-  class Error < Exception; end
-  
 end
