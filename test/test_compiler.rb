8aa6093cb9f327893d856a92eb6957253b2ff933
rasmusrn <rasmusrnielsen@gmail.com>
2011-05-24 02:58:32 +0200

    Added a few more tests.

--- test/test_compiler.rb
+++ test/test_compiler.rb
@@ -4,4 +4,13 @@ class CompilerTest < Reco::TestCase
   def test_compiling_hello_eco_fixture
     assert Reco.compile fixture("hello.eco")
   end
+  
+  def test_compiling_hello_eco_fixture
+    assert Reco.compile fixture("projects.eco")
+  end
+  
+  def test_compiling_hello_eco_fixture
+    assert Reco.compile fixture("helpers.eco")
+  end
+
 end
