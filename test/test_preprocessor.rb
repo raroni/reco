4f4946e7f2e5cf0da9d2ca14456fadc31911a503
rasmusrn <rasmusrnielsen@gmail.com>
2011-05-24 02:04:32 +0200

    Refactoring: DRYed up test_preprocessor a bit.

--- test/test_preprocessor.rb
+++ test/test_preprocessor.rb
@@ -2,25 +2,26 @@ require 'reco_test'
 
 class PreprocessorTest < Reco::TestCase
   def test_preprocessing_hello_eco_fixture
-    assert_equal fixture('hello.coffee'), preprocess(fixture('hello.eco'))
+    assert_equal fixture('hello.coffee'), preprocess('hello.eco')
   end
   
   def test_preprocessing_projects_eco_fixture
-    assert_equal fixture('projects.coffee'), preprocess(fixture('projects.eco'))
+    assert_equal fixture('projects.coffee'), preprocess('projects.eco')
   end
   
   def test_preprocessing_helpers_eco_fixture
-    assert_equal fixture('helpers.coffee'), preprocess(fixture('helpers.eco'))
+    assert_equal fixture('helpers.coffee'), preprocess('helpers.eco')
   end
   
   def test_unexpected_dedent
     assert_raises RuntimeError do
-      preprocess fixture('invalid_dedentation.eco')
+      preprocess 'invalid_dedentation.eco'
     end
   end
   
   private
-  def preprocess(source)
+  def preprocess(path)
+    source = fixture(path)
     Reco::Preprocessor.preprocess source
   end
 end
